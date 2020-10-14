c99c027e08ddab9fa3495869be35ff013b9ee2a6
yogiex <priyagung.yoggi@gmail.com>
2020-10-15 01:45:52 +0700

    routing sejarah

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// * GET home page. 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'WebDup' });
 });
 
 
+// * routing page /aboutus/sejarah
+router.get('/aboutus/sejarah', async (req, res) => {
+  await res.render('sejarah', {title:'webdup'});
+})
 module.exports = router;
